fb47705fee46aee7aab3efc7c5c4c6bf022ff3df
vaishali15 <vaishalic@codeblock.co.in>
2020-08-10 16:07:19 +0530

    hsv code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  modalRef: BsModalRef;
   title = 'hsv';
+  showSingle = false;
+  showTwo = false;
+  changeText: boolean;
+  constructor(private modalService: BsModalService) {
+    this.changeText = false;
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  addSingleElement() {
+    this.modalRef.hide();
+    this.showTwo = false;
+    this.showSingle = true;
+  }
+
+  addTwoElement() {
+    this.modalRef.hide();
+    this.showSingle = false;
+    this.showTwo = true;
+  }
+
+  mouseFunction() {
+    this.showTwo = false;
+    this.showSingle = false;
+  }
+
+
 }
